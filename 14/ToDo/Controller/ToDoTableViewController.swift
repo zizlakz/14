49e9632e5b1c6d6922f94ffed5d43d642c96690c
e <e>
2019-06-06 11:43:14 +0200

    Add a new item

---
+++ 14/ToDo/Controller/ToDoTableViewController.swift
@@ -0,0 +1,31 @@
+
+
+import UIKit
+
+class ToDoTableViewController: UITableViewController {
+        
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        
+        return list.count
+    }
+
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+        cell.textLabel?.text = list[indexPath.row]
+
+
+        return cell
+    }
+    
+    @IBAction func calcelAction(_ segue: UIStoryboardSegue){}
+ 
+
+
+}
